808b932dd048b2fd9bc235e360ad5765390b0a2a
ThabataG <thabata.helen@gmail.com>
2014-11-19 23:12:05 -0200

    Classe principal

--- learning_TDD/src/learning_TDD/Fibonacci.java
+++ learning_TDD/src/learning_TDD/Fibonacci.java
@@ -2,4 +2,14 @@ package learning_TDD;
 
 public class Fibonacci {
 
+	/**
+	 * Generate prime number to i using fibonacci
+	 * @param i generate prime numbers from 1 to i
+	 * @return String with generated prime numbers
+	 */
+	public Object generatorPrimeNumbersto(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
